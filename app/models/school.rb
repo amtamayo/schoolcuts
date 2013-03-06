6fd9410aaa23297ae827a16c9080acc59397145b
elnaz <elnaz.mosh@gmail.com>
2013-03-06 15:16:24 -0600

    first working version since merge issues; had to back out isat chart but isat data is in tact

--- app/models/school.rb
+++ app/models/school.rb
@@ -2,9 +2,6 @@ class School < ActiveRecord::Base
   attr_accessible :access_type, :community_area, :cps_id, :full_name, :latitude, :longitude, :school_type, :short_name, :street_address
   
   has_many :races
-<<<<<<< HEAD
   has_many :isat_scores
-=======
->>>>>>> 035ae6a3d14e8f1c0b14cb2983e76e09048b162e
   has_many :enrollments
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,6 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130306030858) do
-
-  create_table "isat_scores", :force => true do |t|
-    t.integer  "school_id"
-    t.integer  "year_from"
-    t.integer  "year_to"
-    t.string   "subject"
-    t.float    "percent"
-
 ActiveRecord::Schema.define(:version => 20130306030901) do
 
   create_table "enrollments", :force => true do |t|
@@ -27,6 +18,16 @@ ActiveRecord::Schema.define(:version => 20130306030901) do
     t.integer  "year_from"
     t.integer  "year_to"
     t.integer  "count"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  create_table "isat_scores", :force => true do |t|
+    t.integer  "school_id"
+    t.integer  "year_from"
+    t.integer  "year_to"
+    t.string   "subject"
+    t.float    "percent"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
