dd5535a701dde704b5cf597f4060b5485ce29898
Elnaz Moshfeghian <elnaz.mosh@gmail.com>
2013-03-21 02:51:56 -0500

    added commented-out copy for home page; made templates live

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ SchoolClosing::Application.routes.draw do
   resources :schools, :only => [:show]
 
   get "about/index"
+  match '/templates/compare.html', :to => redirect('/templates/compare.html')
 
   root :to => "home#index"
 end
