f333ca8a7001f0a179102903489aaa03a1c8357a
amarie <amtamayo@gmail.com>
2013-03-21 22:43:13 -0500

    Updates to source school data

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,9 @@
 class HomeController < ApplicationController
   def index
-  	@status = 2
+  	@status = 1
   	filter = "closing_status = #{@status}"
-  	prelim_filter = "closing_status = 1"
-  	receiving_filter = "receiving_status = 3"
+  	prelim_filter = "closing_status = 0"
+  	receiving_filter = "receiving_status = 1"
   	relocating_filter = "closing_status = 4"
   	@schools = School.where(filter).sort_by{ |s| s.short_name }
   	@students_affected = @schools.map{|s| s.enrollments.select{|e|e.year_from==2012}}.map{|e| e.first.count }.inject{|sum,x| sum + x}
