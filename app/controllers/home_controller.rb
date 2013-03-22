1b5b2a1c9fcc9fddfdb3f98b5f1924f9aefdc0e1
Elnaz Moshfeghian <elnaz.mosh@gmail.com>
2013-03-21 19:32:45 -0500

    map icons; home page copy set up for new lists;

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,15 @@
 class HomeController < ApplicationController
   def index
-  	@status = 1
+  	@status = 2
   	filter = "closing_status = #{@status}"
+  	prelim_filter = "closing_status = 1"
+  	receiving_filter = "receiving_status = 3"
+  	relocating_filter = "closing_status = 4"
   	@schools = School.where(filter).sort_by{ |s| s.short_name }
+  	@students_affected = @schools.map{|s| s.enrollments.select{|e|e.year_from==2012}}.map{|e| e.first.count }.inject{|sum,x| sum + x}
+  	@prelim_schools = School.where(prelim_filter).sort_by{|s| s.short_name }
+  	@receiving_schools = School.where(receiving_filter).sort_by{|s| s.short_name }
+  	@relocating_schools = School.where(relocating_filter).sort_by{|s| s.short_name }
   	
     respond_to do |format|
       format.html # index.html.erb
