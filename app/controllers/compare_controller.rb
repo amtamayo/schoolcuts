4ce9de44b638fa75ebcd8da83a726c1a4c35092f
amarie <amtamayo@gmail.com>
2013-05-15 14:41:50 -0500

    rollback memcache changes

--- app/controllers/compare_controller.rb
+++ app/controllers/compare_controller.rb
@@ -1,7 +1,7 @@
 class CompareController < ApplicationController
 
   def show
-  	@school = Rails.cache.fetch("compare.school.#{params[:id]}") { School.find(params[:id]) }
-	  @associated_schools = Rails.cache.fetch("compare.associated_schools.#{params[:id]}") { @school.school_actions.sort{|a,b| a.result_code <=> b.result_code}.map{|action| action.result }.uniq(&:id) }
+  	@school = School.find(params[:id])
+  	@associated_schools = @school.school_actions.sort{|a,b| a.result_code <=> b.result_code}.map{|action| action.result }.uniq(&:id)
   end
 end
